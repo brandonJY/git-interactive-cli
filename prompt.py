f731fb518367ae4da98e201e50b8f072e1e5403f
brandon-jiang <brandon.jiang.p@outlook.com>
2016-07-30 17:42:40 +0800

    pop current branchname everytime like git bash
    - replace deprecated os.system with subprocess. use shell=True now, may
    not be ideal according to
    https://docs.python.org/2/library/subprocess.html#frequently-used-arguments
    - add switch branch command

--- prompt.py
+++ prompt.py
@@ -7,7 +7,7 @@ from prompt_toolkit.styles import style_from_dict
 from prompt_toolkit.token import Token
 from prompt_toolkit.history import FileHistory
 
-import os
+import subprocess
 from os.path import expanduser
 
 cli_style = style_from_dict({
@@ -26,6 +26,7 @@ cli_completer = WordCompleter([
     'stash save -p message',
     'checkout -p commitid',
     'checkout branchname filepath',
+    'checkout branchname',
     'commit --amend',
     'rebase -i HEAD~4',
     'rebase -i --root',
@@ -33,6 +34,7 @@ cli_completer = WordCompleter([
     "filter-branch --env-filter 'GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE; export GIT_COMMITTER_DATE",
     "log --graph --abbrev-commit --decorate --format=format:'%C(blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset) %C(bold green)%ar%C(reset)'"
 ], meta_dict={
+	'checkout branchname':'switch branch',
     'remote -v': 'check remote url',
     'rebase -i HEAD~4': 'interative last 4',
     'remote set-url branchname repopath':'remote set-url origin https://github.com/REPOSITORY.git'
@@ -44,6 +46,11 @@ def main():
 	git_history = FileHistory(home+'/.git-inter-cli-history')
 	try:
 		while True:
+	
+			currentBranchName=subprocess.check_output('git name-rev --name-only HEAD', shell=True)
+			currentPath=subprocess.check_output('pwd')
+			print(currentPath.replace('\n','')+' ('+currentBranchName.replace('\n','')+')')
+	
 			text = prompt('$ git ', completer=cli_completer,
 						  style=cli_style,complete_while_typing=True,
 						  mouse_support=True,history=git_history,
@@ -54,8 +61,10 @@ def main():
 					if text=='git':
 						break
 					os.system(text)
+			elif text=='':
+				continue
 			else:
-				os.system("git " + text)
+				status=subprocess.call("git " + text, shell=True)
 	except EOFError:
 		print('exit')
 		
